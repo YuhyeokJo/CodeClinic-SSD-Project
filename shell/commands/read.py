d79dc5b17ff8a9722fa55afcba280b69ca963754
hyewonkim <hyewonds.kim@samsung.com>
2025-08-06 08:45:47 +0900

    [feature] Add ReadCommand Fail Test not_digit

--- shell/commands/read.py
+++ shell/commands/read.py
@@ -6,9 +6,13 @@ class ReadCommand(Command):
     def __init__(self, driver: SSDDriver):
         self._driver = driver
 
-    def execute(self, args: list[str]) -> str:
+    def execute(self, args: list[str])->str:
         if len(args) != 1:
             print("Usage: read <LBA>")
+            return "[READ] INVALID COMMAND"
+
+        if not args[0].isdigit():
+            print("LBA must be a number")
             return "[READ] INVALID COMMAND"
 
         lba = args[0]
--- tests/test_read_command.py
+++ tests/test_read_command.py
@@ -17,6 +17,13 @@ def test_execute_with_too_many_args(mocker):
     assert INVALID_COMMAND == readCommand.execute(['123', '456'])
 
 
+def test_execute_with_not_digit(mocker):
+    ssd_driver = mocker.Mock(spec=SSDDriver)
+
+    readCommand = ReadCommand(ssd_driver)
+    assert INVALID_COMMAND == readCommand.execute(['12e'])
+
+
 def test_execute_with_valid_arg(mocker, capsys):
     ssd_driver = mocker.Mock(spec=SSDDriver)
 
