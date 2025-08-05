b31fa6eaf8c752121b261c6ec115d602be7c3e1d
dhkang <pwannabe2014@gmail.com>
2025-08-05 17:01:44 +0900

    [feature] Read 에서 기록 한적 없는 LBA 를 읽으면 0x00000000 으로 읽힌다 구현

--- device/ssd.py
+++ device/ssd.py
@@ -8,6 +8,7 @@ class SSD(Device):
         self.output_dir = Path(__file__).resolve().parent.parent / "output"
         self.output_dir.mkdir(exist_ok=True)
         self.ssd_nand_file = self.output_dir / "ssd_nand.txt"
+        self.ssd_output_file = self.output_dir / "ssd_output.txt"
 
     def write(self, cmd: str, address: int | str, value: int | str):
         data = {}
@@ -23,4 +24,17 @@ class SSD(Device):
                 f.write(f"{addr} {val}\n")
 
     def read(self, cmd, address):
-        pass
+        """
+            Read 명령어는 ssd_nand.txt에서 데이터를 읽고,
+            읽은데이터를ssd_output.txt 파일에 기록한다.
+        """
+        data = {}
+        if os.path.exists(self.ssd_nand_file):
+            with open(self.ssd_nand_file, "r") as f:
+                for line in f:
+                    addr, val = line.rstrip().split()
+                    data[str(addr)] = val
+
+        result = data.get(address, "0x00000000")
+        with open(self.ssd_output_file, "w") as f:
+            f.write(f"{address} {result}\n")
--- tests/test_ssd.py
+++ tests/test_ssd.py
@@ -73,50 +73,57 @@ def test_write_when_same_address(ssd):
     assert data['2'] == '0xffffffff'
 
 
-def test_read_creates_ssd_output_text_and_read_value():
+def test_read_init_value_should_be_0x00000000(ssd):
+    """
+    기록이 한적이 없는 LBA를 읽으면 0x00000000 으로 읽힌다.
+    """
+    ssd.read("R", 0)
+    data = {}
+    with open(ssd.ssd_output_file, "r") as f:
+        for line in f:
+            address, val = line.rstrip().split(' ')
+            data[str(address)] = val
+
+    assert data["0"] == "0x00000000"
+
+
+def test_read_creates_ssd_output_text_and_read_value(ssd):
     """
     Read 명령어는 ssd_nand.txt에서 데이터를 읽고,
-    읽은데이터를ssd_output.txt 파일에 기록한다.
+    읽은 데이터를 ssd_output.txt 파일에 기록한다.
     """
     ...
 
 
-def test_read_origin_ssd_output_should_be_gone():
+def test_read_origin_ssd_output_should_be_gone(ssd):
     """
      ssd_output.txt 에 읽은 값이 적힌다. (기존 데이터는 사라진다.)
     """
     ...
 
 
-def test_read_init_value_should_be_0x00000000():
-    """
-    기록이 한적이 없는 LBA를 읽으면 0x00000000 으로 읽힌다.
-    """
-    ...
-
-
-def test_read_wrong_lba_print_ERROR_at_ssd_output_txt_if_not_0_99():
+def test_read_wrong_lba_print_ERROR_at_ssd_output_txt_if_not_0_99(ssd):
     """
      • 잘못된LBA 범위가 입력되면ssd_output.txt에 "ERROR"가 기록된다.
     """
     ...
 
 
-def test_write_wrong_lba_print_ERROR_at_ssd_output_txt_if_not_0_99():
+def test_write_wrong_lba_print_ERROR_at_ssd_output_txt_if_not_0_99(ssd):
     """
      • 잘못된LBA 범위가입력되면ssd_output.txt에 "ERROR"가 기록된다.
     """
     ...
 
 
-def test_lba_should_be_decimal():
+def test_lba_should_be_decimal(ssd):
     """
     • [LBA] : 0 ~ 99, 10진수로 입력 받음
     """
     ...
 
 
-def test_value_always_startswith_0x_and_length_10():
+def test_value_always_startswith_0x_and_length_10(ssd):
     """
     [Value] : 항상 0x가 붙으며 10 글자로 표기한다. ( 0x00000000  ~  0xFFFFFFFF)
     """
