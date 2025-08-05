cbd0bee3956b5db85f941dc795c9742a188561e9
BaeJoungEun <wjddms5810@naver.com>
2025-08-05 16:14:45 +0900

    FullWrite Class 생성

--- shell/commands/fullwrite.py
+++ shell/commands/fullwrite.py
@@ -1,5 +1,12 @@
 from shell.command import Command
+from shell.driver import SSDDriver
+
 
 class FullWrite(Command):
+    def __init__(self, driver:SSDDriver):
+        self._driver = driver
+
     def execute(self, args: list[str]) -> None:
+        for LBA in range(0, 99):
+            self._driver.write(str(LBA), args[0])
         return True
--- tests/test_shell.py
+++ tests/test_shell.py
@@ -1,6 +1,11 @@
+from pytest_mock import MockerFixture
+
 from shell.commands.fullwrite import FullWrite
+from shell.driver import SSDDriver
 
 
-def test_full_write_command():
-    full_write_command = FullWrite()
-    assert full_write_command.execute(['fullwrite 0xABCDFFFF'])
+def test_full_write_command(mocker: MockerFixture):
+    driver = mocker.Mock(spec=SSDDriver)
+    driver.write.return_value = True
+    full_write_command = FullWrite(driver)
+    assert full_write_command.execute(['0xABCDFFFF'])
