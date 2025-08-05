c40698455263cee1c7aed1968fd94eed51ac8ab7
hyewonkim <hyewonds.kim@samsung.com>
2025-08-06 08:45:24 +0900

    [feature] Add ReadCommand Fail Test

--- shell/commands/read.py
+++ shell/commands/read.py
@@ -6,7 +6,11 @@ class ReadCommand(Command):
     def __init__(self, driver: SSDDriver):
         self._driver = driver
 
-    def execute(self, args: list[str])->str:
+    def execute(self, args: list[str]) -> str:
+        if len(args) != 1:
+            print("Usage: read <LBA>")
+            return "[READ] INVALID COMMAND"
+
         lba = args[0]
         result = self._driver.read(lba)
         return f"[READ] LBA {lba}: {result}"
--- tests/test_read_command.py
+++ tests/test_read_command.py
@@ -1,6 +1,21 @@
 from shell.commands.read import ReadCommand
 from shell.driver import SSDDriver
 
+INVALID_COMMAND = "[READ] INVALID COMMAND"
+
+def test_execute_with_no_args(mocker):
+    ssd_driver = mocker.Mock(spec=SSDDriver)
+
+    readCommand = ReadCommand(ssd_driver)
+    assert INVALID_COMMAND == readCommand.execute([])
+
+
+def test_execute_with_too_many_args(mocker):
+    ssd_driver = mocker.Mock(spec=SSDDriver)
+
+    readCommand = ReadCommand(ssd_driver)
+    assert INVALID_COMMAND == readCommand.execute(['123', '456'])
+
 
 def test_execute_with_valid_arg(mocker, capsys):
     ssd_driver = mocker.Mock(spec=SSDDriver)
