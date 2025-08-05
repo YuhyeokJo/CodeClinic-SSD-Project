2027d135fa12ad7b3b92adf210ac34bff1d0eca7
hyewonkim <hyewonds.kim@samsung.com>
2025-08-06 08:43:55 +0900

    [feature] Add ReadCommand Class

---
+++ shell/commands/read.py
@@ -0,0 +1,12 @@
+from shell.command import Command
+from shell.driver import SSDDriver
+
+
+class ReadCommand(Command):
+    def __init__(self, driver: SSDDriver):
+        self._driver = driver
+
+    def execute(self, args: list[str])->str:
+        lba = args[0]
+        result = self._driver.read(lba)
+        return f"[READ] LBA {lba}: {result}"
