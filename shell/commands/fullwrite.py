b2ba306f4499e14c414458d25a3b301b3e774349
BaeJoungEun <wjddms5810@naver.com>
2025-08-05 16:35:19 +0900

    FullWrite invalid 추가

--- shell/commands/fullwrite.py
+++ shell/commands/fullwrite.py
@@ -1,3 +1,5 @@
+import re
+
 from shell.command import Command
 from shell.driver import SSDDriver
 
@@ -7,6 +9,19 @@ class FullWrite(Command):
         self._driver = driver
 
     def execute(self, args: list[str]) -> None:
-        for LBA in range(0, 99):
-            self._driver.write(str(LBA), args[0])
+        if len(args) != 1:
+            print("Usage: fullwrite <DATA_PATTERN>")
+            return False
+
+        data = args[0]
+        if not re.fullmatch(r'0x[0-9A-Fa-f]+', data):
+            print("Error: DATA must be a valid hex (0x + 0~9A~F)")
+            return False
+
+        for LBA in range(0, 100):
+            success = self._driver.write(str(LBA), data)
+            if not success:
+                print(f"Write failed at LBA {LBA}")
+                return False
+
         return True
--- tests/test_shell.py
+++ tests/test_shell.py
@@ -1,11 +1,31 @@
+import pytest
 from pytest_mock import MockerFixture
 
 from shell.commands.fullwrite import FullWrite
 from shell.driver import SSDDriver
 
 
-def test_full_write_command(mocker: MockerFixture):
+def test_full_write_command_success(mocker: MockerFixture):
     driver = mocker.Mock(spec=SSDDriver)
     driver.write.return_value = True
     full_write_command = FullWrite(driver)
+
     assert full_write_command.execute(['0xABCDFFFF'])
+    assert driver.write.call_count == 100
+
+    called_lbas = [call.args[0] for call in driver.write.call_args_list]
+    assert called_lbas == [str(x) for x in range(100)]
+
+def test_full_write_command_fail_empty_data(mocker: MockerFixture):
+    driver = mocker.Mock(spec=SSDDriver)
+    driver.write.return_value = False
+    full_write_command = FullWrite(driver)
+
+    assert full_write_command.execute([]) is False
+
+def test_full_write_command_fail_wrong_data(mocker: MockerFixture):
+    driver = mocker.Mock(spec=SSDDriver)
+    driver.write.return_value = False
+    full_write_command = FullWrite(driver)
+
+    assert full_write_command.execute(['0xAABCDFFFFFFF']) is False
