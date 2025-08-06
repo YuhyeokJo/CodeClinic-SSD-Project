34ccb7fdaa7fc2f7cafd08365992a97cc522d3f2
BaeJoungEun <wjddms5810@naver.com>
2025-08-06 11:07:12 +0900

    [feature] help command - read, write, fullread, fullwrite, exit 에 사용 방법 추가

--- tests/test_help_command.py
+++ tests/test_help_command.py
@@ -5,8 +5,44 @@ from shell.commands.help import Help
 from shell.driver import SSDDriver
 
 
-def test_help_command_success(mocker: MockerFixture):
+def test_help_read_command_success(mocker: MockerFixture):
     driver = mocker.Mock(spec=SSDDriver)
 
     help_command = Help(driver)
-    help_command.execute([]) == "[Help]"
+    assert help_command.execute(['read']) == "read <LBA> <Value>: Reads value from given LBA"
+
+
+def test_help_write_command_success(mocker: MockerFixture):
+    driver = mocker.Mock(spec=SSDDriver)
+
+    help_command = Help(driver)
+    assert help_command.execute(['Write']) == "write <LBA>: Writes value to LBA"
+
+
+def test_help_fullread_command_success(mocker: MockerFixture):
+    driver = mocker.Mock(spec=SSDDriver)
+
+    help_command = Help(driver)
+    assert help_command.execute(['fullread']) == "fullread: Reads value from all LBA(0~99)"
+
+
+def test_help_fullwrite_command_success(mocker: MockerFixture):
+    driver = mocker.Mock(spec=SSDDriver)
+
+    help_command = Help(driver)
+    assert help_command.execute(['fullwrite']) == "fullwrite <Value>:  Writes value to all LBA(0~99)"
+
+
+def test_help_unknown_command_success(mocker: MockerFixture):
+    driver = mocker.Mock(spec=SSDDriver)
+
+    help_command = Help(driver)
+    assert help_command.execute(['red']) == f"Unknown command: red"
+
+
+def test_help_empty_command_success(mocker: MockerFixture):
+    driver = mocker.Mock(spec=SSDDriver)
+
+    help_command = Help(driver)
+    assert help_command.execute(['red']) == f"Unknown command: red"
+
