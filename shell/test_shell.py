00836f6211f7f764425bc937258493f67bbe66f8
Cho-Bogeun <bog9989@gmail.com>
2025-08-06 10:30:27 +0900

    [feature] read command 한 가지만 처리하는 run 구현

--- shell/test_shell.py
+++ shell/test_shell.py
@@ -1,8 +1,8 @@
 from shell.command import Command
-from shell.commands.fullread import Fullread
+from shell.commands.fullread import FullRead
 from shell.commands.fullwrite import FullWrite
-from shell.commands.read import ReadCommand
-from shell.commands.write import WriteCommand
+from shell.commands.read import Read
+from shell.commands.write import Write
 from shell.driver import SSDDriver
 
 
@@ -13,9 +13,18 @@ class TestShell:
         self._register_builtin_commands()
 
     def _register_builtin_commands(self):
-        self._commands["write"] = WriteCommand(self._driver)
-        self._commands["read"] = ReadCommand(self._driver)
+        self._commands["write"] = Write(self._driver)
+        self._commands["read"] = Read(self._driver)
         self._commands["fullwrite"] = FullWrite(self._driver)
-        self._commands["fullread"] = Fullread(self._driver)
+        self._commands["fullread"] = FullRead(self._driver)
 
+    def run(self):
+        line = input("Shell> ").strip()
+        if not line:
+            return
+        parts = line.split()
+        cmd = parts[0]
+        args = parts[1:]
 
+        if cmd in self._commands:
+            print(self._commands[cmd].execute(args))
