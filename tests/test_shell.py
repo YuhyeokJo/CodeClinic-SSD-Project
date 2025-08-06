3bae7a95f80ee59397ac8ed9df4882ccaa1f45d4
Cho-Bogeun <bog9989@gmail.com>
2025-08-06 11:15:56 +0900

    [refactor] 테스트를 fixture로 리팩터링

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -8,6 +8,13 @@ from shell.commands.write import Write
 from shell.driver import SSDDriver
 from shell.test_shell import TestShell
 
+@pytest.fixture
+def mocked_driver_shell_input(mocker: MockerFixture):
+    driver = mocker.Mock(spec=SSDDriver)
+    test_shell = TestShell(driver)
+    input_patch = mocker.patch("shell.test_shell.input")
+
+    return driver, test_shell, input_patch
 
 def test_if_builtin_commands_are_registered(mocker: MockerFixture):
     driver = mocker.Mock(spec=SSDDriver)
@@ -19,11 +26,9 @@ def test_if_builtin_commands_are_registered(mocker: MockerFixture):
     assert isinstance(test_shell._commands["fullwrite"], FullWrite)
     assert isinstance(test_shell._commands["fullread"], FullRead)
 
-def test_read_command_input_with_correct_command(capsys, mocker: MockerFixture):
+def test_read_command_input_with_correct_command(capsys, mocked_driver_shell_input):
     # Arrange
-    driver = mocker.Mock(spec=SSDDriver)
-    test_shell = TestShell(driver)
-    input_patch = mocker.patch("shell.test_shell.input")
+    driver, test_shell, input_patch = mocked_driver_shell_input
     driver.read.return_value = "0x00000001"
     input_patch.side_effect = ["read 3", "exit"]
 
@@ -35,11 +40,9 @@ def test_read_command_input_with_correct_command(capsys, mocker: MockerFixture):
     assert last_shell_line == "[Read] LBA 3: 0x00000001"
 
 
-def test_read_command_input_with_wrong_lba(capsys, mocker: MockerFixture):
+def test_read_command_input_with_wrong_lba(capsys, mocked_driver_shell_input):
     # Arrange
-    driver = mocker.Mock(spec=SSDDriver)
-    test_shell = TestShell(driver)
-    input_patch = mocker.patch("shell.test_shell.input")
+    driver, test_shell, input_patch = mocked_driver_shell_input
     driver.read.return_value = "0x00000001"
     input_patch.side_effect = ["read -1", "exit"]
 
@@ -51,11 +54,9 @@ def test_read_command_input_with_wrong_lba(capsys, mocker: MockerFixture):
     assert last_shell_line == "[Read] INVALID COMMAND"
 
 
-def test_read_command_correctly_until_exit(capsys, mocker: MockerFixture):
+def test_read_command_correctly_until_exit(capsys, mocked_driver_shell_input):
     # Arrange
-    driver = mocker.Mock(spec=SSDDriver)
-    test_shell = TestShell(driver)
-    input_patch = mocker.patch("shell.test_shell.input")
+    driver, test_shell, input_patch = mocked_driver_shell_input
     driver.read.side_effect = ["0x00000003"]
     input_patch.side_effect = ["read 3", "exit"]
 
