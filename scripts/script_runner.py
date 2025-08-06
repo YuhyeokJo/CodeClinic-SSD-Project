833b153e95e614597aa4250add406de03174f16b
yuhyeok <joyu0320@naver.com>
2025-08-06 16:00:36 +0900

    [bugfix] test script 1 loop 수정

--- scripts/script_runner.py
+++ scripts/script_runner.py
@@ -24,6 +24,8 @@ class ScriptRunner:
                 res = self.write_command.execute([lba, inputs[lba]])
                 if INVALID_COMMAND in res:
                     return INVALID_COMMAND
+        for group in groups:
+            for lba in group:
                 results.append(self._read_compare([lba, inputs[lba]]))
         if not all(results):
             return SCRIPT_FAIL
