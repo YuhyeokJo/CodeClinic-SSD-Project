5cdc01ace82c10ed5f09c81729c221a944122ba7
BaeJoungEun <wjddms5810@naver.com>
2025-08-06 11:07:12 +0900

    [feature] help command - read, write, fullread, fullwrite, exit 에 사용 방법 추가

--- shell/commands/help.py
+++ shell/commands/help.py
@@ -3,9 +3,23 @@ from shell.driver import SSDDriver
 
 
 class Help(Command):
-    def __init__(self, driver:SSDDriver):
+    def __init__(self, driver: SSDDriver):
         self._driver = driver
+        self._help_map = {
+            "READ": "read <LBA> <Value>: Reads value from given LBA",
+            "WRITE": "write <LBA>: Writes value to LBA",
+            "FULLREAD": "fullread: Reads value from all LBA(0~99)",
+            "FULLWRITE": "fullwrite <Value>:  Writes value to all LBA(0~99)",
+            "EXIT": "exit: Exit shell"
+        }
 
     def execute(self, args: list[str]) -> str:
-        # self._driver.close()
-        return "[Help]"
+        if not args:
+            available = "\n".join(f"- {cmd}" for cmd in self._help_map)
+            return f"Available commands:\n{available}"
+
+        cmd = args[0]
+        cmd_upper = cmd.upper()
+        if cmd_upper in self._help_map:
+            return self._help_map[cmd_upper]
+        return f"Unknown command: {cmd}"
