28c615b885bc4a478f5de60c651fffedb620a59e
hyewonkim <hyewonds.kim@samsung.com>
2025-08-06 16:06:48 +0900

    [feature] Add test_driver-tc

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -1,17 +1,39 @@
-from pytest_mock import mocker
+from pathlib import Path
 from shell.driver import SSDDriver
 
 
-def test_driver_read(mocker):
+def test_driver_read_mock(mocker):
     ssd_driver = mocker.Mock(spec=SSDDriver)
     ssd_driver.read.return_value = "[Read] LBA 10: 0x12345678"
     result = ssd_driver.read("10")
 
     assert result == "[Read] LBA 10: 0x12345678"
 
-def test_driver_write(mocker):
+
+def test_driver_write_mock(mocker):
     ssd_driver = mocker.Mock(spec=SSDDriver)
     ssd_driver.write.return_value = f"[Write] Done"
     result = ssd_driver.write("10", "0x12345678")
 
     assert result == f"[Write] Done"
+
+
+def test_driver_read():
+    output_dir = Path(__file__).resolve().parent.parent / "output"
+    ssd_nand_file = output_dir / "ssd_nand.txt"
+    with open(ssd_nand_file, "w") as f:
+        f.write("1 0x12345678")
+
+    ssd_driver = SSDDriver()
+    result = ssd_driver.read("1")
+
+    assert result == "0x12345678"
+
+
+def test_driver_read_write():
+    ssd_driver = SSDDriver()
+    assert ssd_driver.write("10", "0xAABBCCDD")
+
+    result = ssd_driver.read("10")
+
+    assert result == "0xAABBCCDD"
