c70840039ae661a783b0208218f8cf9f0e74d6c8
yuhyeok <joyu0320@naver.com>
2025-08-06 10:40:44 +0900

    [refactor] 문자열 내용 상수로 변경

--- shell/commands/script.py
+++ shell/commands/script.py
@@ -3,19 +3,19 @@ from shell.command import Command
 from shell.commands.write import Write
 from shell.commands.read import Read
 
+INVALID =  "INVALID"
+SCRIPT_INVALID_COMMAND = "[SCRIPT] INVALID COMMAND"
 
 class Script(Command):
     def __init__(self, driver):
         self.write_command = Write(driver)
         self.read_command = Read(driver)
 
-
     def execute(self, args: list[str]) -> str:
         res = self.write_command.execute(args)
-        if "INVALID" in res:
-            return "[SCRIPT] INVALID COMMAND"
+        if INVALID in res:
+            return SCRIPT_INVALID_COMMAND
         self._read_comapre(args)
 
     def _read_comapre(self, args):
         pass
-
--- tests/test_script_runner.py
+++ tests/test_script_runner.py
@@ -23,14 +23,15 @@ def test_execute_with_not_digit(mocker: MockerFixture):
     assert INVALID_COMMAND == script.execute(['12e'])
 
 
-def test_execute_write(mocker: MockerFixture):
+# def test_execute_write(mocker: MockerFixture):
+#     driver = mocker.Mock(spec=SSDDriver)
+#     script = Script(driver)
+#     script.execute(["3", "0x00000001"])
+#     assert driver.write.call_count == 1
+
+
+def test_execute_with_valid_arg(mocker, capsys):
     driver = mocker.Mock(spec=SSDDriver)
     script = Script(driver)
-    script.execute(["3", "0x00000001"])
-    assert driver.write.call_count == 1
-
-# def test_execute_with_valid_arg(mocker, capsys):
-#     driver = mocker.Mock(spec=SSDDriver)
-#     script = Script(driver)b
-#     script.read_compare.return_value = "0x12345678"
-#     assert "[SCRIPT] LBA 42: 0x12345678" == script.execute(['42'])
+    driver.read.return_value = "0x12345678"
+    assert "[SCRIPT] PASS" == script.execute(["3", "0x12345678"])
