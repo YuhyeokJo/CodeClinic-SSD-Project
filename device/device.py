20802e344525e20bdddef4883f757dbed6f3fc4a
dhkang <pwannabe2014@gmail.com>
2025-08-07 14:42:48 +0900

    [refactor] device 에 erase abstractmethod 추가

--- device/device.py
+++ device/device.py
@@ -3,9 +3,13 @@ from abc import ABC, abstractmethod
 
 class Device(ABC):
     @abstractmethod
-    def write(self, address, value):
+    def write(self, lba: str, value: str):
         pass
 
     @abstractmethod
-    def read(self, address):
+    def read(self, lba: str):
+        pass
+
+    @abstractmethod
+    def erase(self, lba: str, size: str) -> None:
         pass
