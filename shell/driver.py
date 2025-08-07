d718a35eb98dc043e2656e0c1c29288a8b08ec13
yuhyeok <joyu0320@naver.com>
2025-08-07 14:20:48 +0900

    [refactor] erase_range에서 erase command를 사용하도록 구현 변경

--- shell/driver.py
+++ shell/driver.py
@@ -5,7 +5,7 @@ from pathlib import Path
 class SSDDriver:
     def read(self, lba: str):
         subprocess.run(
-            f"python {Path(__file__).parent.parent/'device/ssd.py'} R {lba}",
+            f"python {Path(__file__).parent.parent / 'device/ssd.py'} R {lba}",
             shell=True
         )
 
@@ -18,7 +18,7 @@ class SSDDriver:
 
     def write(self, lba: str, data: str):
         subprocess.run(
-            f"python {Path(__file__).parent.parent/'device/ssd.py'} W {lba} {data}",
+            f"python {Path(__file__).parent.parent / 'device/ssd.py'} W {lba} {data}",
             shell=True
         )
 
@@ -27,15 +27,6 @@ class SSDDriver:
     def erase(self, lba: str, size: str):
         subprocess.run(
             f"python {Path(__file__).parent.parent / 'device/ssd.py'} E {lba} {size}",
-            shell=True
-        )
-
-        return True
-
-    def erase_range(self, start_lba: str, end_lba: str):
-        size = int(end_lba) - int(start_lba) + 1
-        subprocess.run(
-            f"python {Path(__file__).parent.parent / 'device/ssd.py'} E {start_lba} {str(size)}",
             shell=True
         )
 
