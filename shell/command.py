b19ef80729dfc2d053526d50af4eb64e8f37438c
BaeJoungEun <wjddms5810@naver.com>
2025-08-08 10:27:13 +0900

    [feature] Command에 logger 추가

--- shell/command.py
+++ shell/command.py
@@ -1,12 +1,9 @@
 from abc import ABC, abstractmethod
-from logger import Logger
+from shell.logger import Logger
 
 
 class Command(ABC):
     _logger = Logger()
-
-    def __init__(self, name: str):
-        self._name = name
 
     def log(self, func: str, msg: str):
         self._logger.print(func, msg)
