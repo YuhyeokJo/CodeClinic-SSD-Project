a5c65a0a2ab0a55e7113eebee8822c6df78c6444
Cho-Bogeun <bog9989@gmail.com>
2025-08-08 09:50:00 +0900

    [refactor] 테스크 코드 parametrize

--- tests/test_command_validator.py
+++ tests/test_command_validator.py
@@ -2,15 +2,14 @@ import pytest
 
 from shell.command_validator import is_valid_lba
 
-def test_if_lba_validate_correct_number():
-    assert is_valid_lba("1") == True
-    assert is_valid_lba("01") == True
-    assert is_valid_lba("11") == True
+@pytest.mark.parametrize(
+    "correct_string", ["1", "01", "11"]
+)
+def test_if_lba_validate_correct_number(correct_string):
+    assert is_valid_lba(correct_string) == True
 
-def test_if_lba_validate_wrong_number_as_false():
-    assert is_valid_lba("-1") == False
-    assert is_valid_lba("1.0") == False
-    assert is_valid_lba("100") == False
-
-def test_if_lba_validate_unicode_number_as_false():
-    assert is_valid_lba("①②③") == False
+@pytest.mark.parametrize(
+    "wrong_string", ["-1", "1.0", "100", "①②③"]
+)
+def test_if_lba_validate_wrong_number_as_false(wrong_string):
+    assert is_valid_lba(wrong_string) == False
